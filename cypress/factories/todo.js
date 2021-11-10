4672ce50b77bb2b1b24d49991aa0a8a52cdcd0cf
Caleb McQuaid <calebmcquaid@gmail.com>
2021-11-10 00:57:49 -0500

    Cypress setup and tests

--- cypress/factories/todo.js
+++ cypress/factories/todo.js
@@ -1,6 +1,5 @@
 const { Factory } = require('fishery')
 
-module.exports = Factory.define(({ sequence }) => ({
-  id: `${sequence}`,
-  title: `Todo #${sequence}`,
-}))
+module.exports = Factory.define(({ sequence }) => (
+  `Todo #${sequence}`
+))
