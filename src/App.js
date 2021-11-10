c8121b935eb7dad43a1f9403625c6f6942972642
Caleb McQuaid <calebmcquaid@gmail.com>
2021-11-10 01:04:00 -0500

    add todo functionality

--- src/App.js
+++ src/App.js
@@ -1,32 +1,26 @@
 import React, {useState} from 'react'
-import TodoList from './components/TodoList/index'
-import AddTodo from './components/AddTodo/index'
+import TodoList from './TodoList/index'
+import {StyledInput, SubmitButton} from './global-styles'
 
 function App() {
-  const [todos, setTodos] = useState([{id: 1, title: 'reaaaaaaaaalllllllllllllllllyyyyyyyyyyyyy longggggggggggg todo'}, {id: 2, title: 'newest todo'}])
+  const [todos, setTodos] = useState([])
   const [currentTodo, setCurrentTodo] = useState('')
 
   function handleSubmit(e) {
     e.preventDefault()
-    console.log({todos})
-    console.log({currentTodo})
-    setTodos([todoObjectFactory, ...todos])
+    setTodos([currentTodo, ...todos])
+    setCurrentTodo('')
 }
 
-  const todoObjectFactory = {
-    id: 5,
-    title: currentTodo
-  }
   return (
     <div>
       <TodoList todos={todos} setTodos={setTodos} />
-      <div>
-          <form onSubmit={(e) => handleSubmit(e)}>
-              <input onChange={(e) => setCurrentTodo(e.target.value)} value={currentTodo} />
-              <input type='submit' value='submit'/>
-          </form>
-          {currentTodo}
-        </div>
+      <div style={{display: 'flex', justifyContent: 'center', marginTop: '20px'}}>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <StyledInput style={{marginRight: '10px'}} onChange={(e) => setCurrentTodo(e.target.value)} value={currentTodo} placeholder="Create a todo" />
+          <SubmitButton data-testid='submit-todo' type='submit' value='Submit' />
+        </form>
+      </div>
     </div>
   );
 }
---
+++ src/global-styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const StyledInput = styled.input`
+    border-radius: 5px;
+    padding: 10px 5px;
+    border: 1px solid grey;
+    width: 300px;
+`
+export const SubmitButton = styled.input`
+    width: 75px;
+    border-radius: 5px;
+    padding: 10px 5px;
+    border: 1px solid grey
+`
